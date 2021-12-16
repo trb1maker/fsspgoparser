e2b1c8612d932b280749691215167661c06f4c0d
trb1maker <tsiporinalex@gmail.com>
2021-12-16 13:20:52 +0500

    Исправление в regexp document: добавлены круглые скобки

--- regexp.go
+++ regexp.go
@@ -6,7 +6,7 @@ var (
 	personName = regexp.MustCompile(`^([А-ЯЁ]+) ([А-ЯЁ]+) ([А-ЯЁ]+) *([\d.]*) *([А-Я.,\s-]*?)$`)
 	legalName  = regexp.MustCompile(`^(.+?),(.+)$`)
 	production = regexp.MustCompile(`^(.+-ИП) от (\d{2}\.\d{2}\.\d{4}) *(.*)$`)
-	document   = regexp.MustCompile(`^(.+) от (\d{2}\.\d{2}\.\d{4}) № ([А-Я ]{0,3}[№ ]*[\d\-\/А-Я,]+) (.+)$`)
+	document   = regexp.MustCompile(`^(.+) от (\d{2}\.\d{2}\.\d{4}) № ([А-Я ]{0,3}[№ ]*[\d-/А-Я,()]+) (.+)$`)
 	subject    = regexp.MustCompile(`(.+?): ([\d.]+) руб\.`)
 	department = regexp.MustCompile(`^(.+?) \d`)
 	bailiff    = regexp.MustCompile(`([А-ЯЁ]+) ([А-ЯЁ]\.) ([А-ЯЁ]\.)<br>(\+\d+)`)
